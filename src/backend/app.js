d72b5db6c43f21d22d3c4b8e58c2d040208f8cd4
gabbyMill <gabbymillatiner@gmail.com>
2021-11-02 18:14:59 +0200

    more clean up + static serving of dist folder + routing of get/ to deliver user with shortened url

--- src/backend/app.js
+++ src/backend/app.js
@@ -6,14 +6,18 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
 const shortUrlRoute = require("./routes/shortUrlRoute.js");
-const serveClientFiles = require("./routes/serveClientFiles.js");
+const getShortenedUrl = require("./routes/getShortened.js");
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/", serveClientFiles);
+app.use("/get", getShortenedUrl);
 app.use("/api/shorturl", shortUrlRoute);
-app.use(express.static("static"));
+
+app.use("/", express.static(path.resolve("./dist")));
+app.get("/", function (req, res) {
+  res.sendFile(path.resolve("./dist/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
