df7027529951d2f02b0ad1cc9f8e9e136e5bec1e
gabbyMill <gabbymillatiner@gmail.com>
2021-11-04 23:16:57 +0200

    Changed place of error handler in app.js

--- src/backend/app.js
+++ src/backend/app.js
@@ -13,12 +13,19 @@ app.use(express.json());
 app.use("/", getShortenedUrl);
 app.use("/api", shortUrlRoute);
 
+app.use(errorHandler);
+
 app.use("/app", express.static(path.resolve("./dist")));
 app.get("/app", function (req, res) {
   res.sendFile(path.resolve("./dist/index.html"));
 });
-app.use(errorHandler);
+
 app.listen(process.env.PORT, () => {
   console.log(`App is running on port ${process.env.PORT}`);
 });
 // COMMENT error handler needs optimizing
+
+// 92-941a6345d75e fwd="109.65.0.215" dyno=web.1 connect=0ms service=8ms status=302 bytes=275 protocol=https
+// 2021-11-04T21:09:15.195324+00:00 app[web.1]: in serve client route
+// 2021-11-04T21:09:45.195018+00:00 heroku[router]: at=error code=H12 desc="Request timeout" method=GET path="/app" host=gabby-mill-shortener.herokuapp.com request_id=c01bb5b7-5df4-4d9e-a36f-d8faa3f0d41c fwd="109.65.0.215" dyno=web.1 connect=0ms service=30000ms status=503 bytes=0 protocol=https
+// 2021-11-04T21:09:46.026209+00:00 app[web.1]: in serve client route
