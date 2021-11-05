0c276f4cb6b99992fc5c3406dad3de415cc8c71a
gabbyMill <gabbymillatiner@gmail.com>
2021-11-05 18:26:26 +0200

    /app first

--- src/backend/app.js
+++ src/backend/app.js
@@ -10,13 +10,13 @@ const errorHandler = require("./middleware/errorHandler.js");
 app.use(cors());
 app.use(express.json());
 
-app.use("/", getShortenedUrl);
 app.use("/api", shortUrlRoute);
 
-app.use("/app/", express.static(path.resolve("./dist")));
-app.get("/app/", function (req, res) {
+app.use("/app", express.static(path.resolve("./dist")));
+app.get("/app", function (req, res) {
   res.sendFile(path.resolve("./dist/index.html"));
 });
+app.use("/", getShortenedUrl);
 app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
