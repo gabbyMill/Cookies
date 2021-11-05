0a52a84512e44b05f9fb0e2ce1cf505c6f6a82bd
gabbyMill <gabbymillatiner@gmail.com>
2021-11-05 11:27:05 +0200

    Deleted frontend other than dist

--- src/frontend/classHandlers/toggleHidden.js
+++
@@ -1,7 +0,0 @@
-export default function makesSureIsNotHidden(className) {
-  const elClasses = document.querySelector(`.${className}`);
-  if ([...elClasses.classList].includes("hidden")) {
-    elClasses.classList.remove("hidden");
-  }
-  return;
-}
--- src/frontend/eventHandlers/sendUrlThroughEnter.js
+++
@@ -1,8 +0,0 @@
-import sendUrl from "./submit";
-const urlBar = document.querySelector(".url-bar");
-export default function sendUrlThroughEnter(keyEvent) {
-  if (keyEvent.key === "Enter") {
-    urlBar.blur();
-    sendUrl(keyEvent);
-  }
-}
