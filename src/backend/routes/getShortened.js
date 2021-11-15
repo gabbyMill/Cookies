10861d9f2f71f4ef7311654718e26078b63fbbf4
gabbyMill <gabbymillatiner@gmail.com>
2021-11-15 15:57:02 +0200

    Added Verify Route

--- src/backend/routes/getShortened.js
+++ src/backend/routes/getShortened.js
@@ -4,6 +4,25 @@ const router = express.Router();
 const User = require("../../../models/User.js");
 const jwt = require("jsonwebtoken");
 
+router.post("/sign", (req, res) => {
+  const { user } = req.body;
+  const secTok = process.env.SEC_TOK;
+  const session = jwt.sign(user, secTok); // { expiresIn: "10s" }
+  // document.sessionStorage.setItem("session1", session);
+  res.json(session);
+});
+router.get("/verify/:id", (req, res, next) => {
+  console.log(1);
+  const { id } = req.params;
+  const secTok = process.env.SEC_TOK;
+  jwt.verify(id, secTok, (err, user) => {
+    if (err) return res.status(403).json("Not able to verify user");
+    req.user = user;
+    res.json(user);
+    // next();
+    // res.status(200).json(user);
+  });
+});
 const { incrementRedirect } = require("../helpers/jsonHandler.js");
 // this route will serve the clients its
 // corresponding url to the generated ids
